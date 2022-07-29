241124cbe55c38dffe5d7fc3f823edfb5072422a
ramkumar <ramviru9@gmail.com>
2022-07-29 23:43:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,18 +10,50 @@ import FoodMenu from './FoodMenu';
 import Register from './Register';
 import Login from './Login';
 import AddFoods from './AddFood';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 import Wishlist from './Wishlist';
 import Admin from './Admin';
 import Editfood from './Editfood';
 
+function reducer(state,action){
+  console.log(action)
+     switch (action.type) {
+      
+      case "Add_To_Cart":
+        return{
+        ...state,cartitems:[...state.cartitems,action.item],
+    total:state.total + parseInt(action.item.foodprice)
+        }
+        
+    case "Remove_From_Cart":
+         let removeCartitem=state.cartitems.filter(obj => action.item._id !==obj._id);
+      // state.cartitems.splice(remove,1);
+      return{
+        cartitems:[...removeCartitem],
+    total:state.total - parseInt(action.item.foodprice)
+        }
+        case "Add_To_Wishlist":
+          return{
+          ...state,wishList:[...state.wishList,action.list]
+          }
+      case "Remove_From_Wishlist":
+           let removeList=state.wishList.filter(e => action.list._id !== e._id);
+        // state.cartitems.splice(remove,1);
+        return{
+          wishList:[...removeList]
+          }
+     }
+     return state;
+}
+
 
 function App() {
+  
   let[foodItems,setFoodItems]=useState([]);
-  let[cartitems,setNewcartitems]=useState([]);
-  let[total,setTotal]=useState(0);
-  let[wishList,setNewwishList]=useState([]);
+  // let[cartitems,setNewcartitems]=useState([]);
+  // let[total,setTotal]=useState(0);
+  // let[wishList,setNewwishList]=useState([]);
   useEffect(() => {
       async function getFooditems(){
           try {
@@ -38,44 +70,67 @@ function App() {
       }
       getFooditems();
   },[])
+  let[state,dispatch]=useReducer(reducer,{cartitems:[],total:0,wishList:[]})
+  let Addtocart=(item) => {
+   dispatch({type:"Add_To_Cart",item})
+    // setNewcartitems([...cartitems,item]);
+    // setTotal(total + parseInt(item.foodprice));
+    }
+    
+    let RemoveFromCart=(item) => {
+      dispatch({type:"Remove_From_Cart",item})
+    //   let remove=cartitems.findIndex(obj => item._id===obj._id);
+    //   cartitems.splice(remove,1);
+    //   setNewcartitems([...cartitems]);
+    //  setTotal(total - parseInt(item.foodprice));
+    }
+    
+    let Addwishlist=(list) => {
+      dispatch({type:"Add_To_Wishlist",list})
+    }
+    
+    let Removewishlist=(list) => {
+      dispatch({type:"Remove_From_Wishlist",list})
+    }
 
-let Addtocart=(item) => {
-setNewcartitems([...cartitems,item]);
-setTotal(total + parseInt(item.foodprice));
-}
 
-let RemoveFromCart=(item) => {
-  let remove=cartitems.findIndex(obj => item._id===obj._id);
-  cartitems.splice(remove,1);
-  setNewcartitems([...cartitems]);
- setTotal(total - parseInt(item.foodprice));
-}
+// let Addtocart=(item) => {
+// setNewcartitems([...cartitems,item]);
+// setTotal(total + parseInt(item.foodprice));
+// }
 
-let Addwishlist=(list) => {
-  setNewwishList([...wishList,list]);
-}
+// let RemoveFromCart=(item) => {
+//   let remove=cartitems.findIndex(obj => item._id===obj._id);
+//   cartitems.splice(remove,1);
+//   setNewcartitems([...cartitems]);
+//  setTotal(total - parseInt(item.foodprice));
+// }
 
-let Removewishlist=(list) => {
-  let index=wishList.findIndex(obj => obj._id === list._id);
-  wishList.splice(index,1);
-  setNewwishList([...wishList])
-}
+// let Addwishlist=(list) => {
+//   setNewwishList([...wishList,list]);
+// }
+
+// let Removewishlist=(list) => {
+//   let index=wishList.findIndex(obj => obj._id === list._id);
+//   wishList.splice(index,1);
+//   setNewwishList([...wishList])
+// }
 
   return (
     <BrowserRouter>
     <div className="App">
       <header className=''>
-        <Navbar Addcartitems={cartitems}></Navbar>
+        <Navbar Addcartitems={state.cartitems}></Navbar>
       </header>
       <main className=''> 
       <Routes>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        <Route path="/home" element={<Home Fooditems={foodItems} Addcart={Addtocart} Addwishlist={Addwishlist} addwish={wishList}/>}></Route>
-        <Route path="/cart" element={<Cart Addcartitems={cartitems} Removecartitems={RemoveFromCart} Total={total}/>}></Route>
+        <Route path="/home" element={<Home Fooditems={foodItems} Addcart={Addtocart} Addwishlist={Addwishlist} addwish={state.wishList}/>}></Route>
+        <Route path="/cart" element={<Cart Addcartitems={state.cartitems} Removecartitems={RemoveFromCart} Total={state.total}/>}></Route>
         <Route path="/foodmenu" element={<FoodMenu Fooditems={foodItems} Addcart={Addtocart}/>}></Route>
         <Route path="/addmenu" element={<AddFoods/>}></Route>
-        <Route path="/wishlist" element={<Wishlist addwish={wishList} Removewishlist={Removewishlist}/>}></Route>
+        <Route path="/wishlist" element={<Wishlist addwish={state.wishList} Addcart={Addtocart} Removewishlist={Removewishlist}/>}></Route>
         <Route path="/admin" element={<Admin/>}></Route>
         <Route path="/editfood/:id" element={<Editfood/>}></Route>
       </Routes>
