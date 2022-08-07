b021ca3939f50d2698876e593581801449d82e7e
ramkumar <ramviru9@gmail.com>
2022-08-07 23:37:01 +0530

    commit

--- src/Footer.js
+++ src/Footer.js
@@ -10,31 +10,45 @@ function Footer() {
         </div>
         </div>
         <div className='row text-uppercase fs-6 mb-0'>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <p>About sanburg</p>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <p>Sanburg branches</p>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <p>For Restaurents</p>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <p>Learn more</p>
         </div>
         </div>
         <div className='row mt-0 text-capitalize text-secondary'>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <a href="#">who we are</a>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <a href="#">Chennai</a>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <a href="#">partner with us</a>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-3 col-lg-3'>
           <a href="#">privacy</a>
+        </div>
+        </div>
+        <div className='row mt-0 text-capitalize text-secondary'>
+        <div className='col-3 col-lg-3'>
+          <a href="#">contact us</a>
+        </div>
+        <div className='col-3 col-lg-3'>
+          <a href="#">bangalore</a>
+        </div>
+        <div className='col-3 col-lg-3'>
+          <a href="#">apps for you</a>
+        </div>
+        <div className='col-3 col-lg-3'>
+          <a href="#">terms</a>
         </div>
         </div>
         </div>
